c38b0016baa5161fe0ea9a90c2385d55a181dc92
Adam <adam.tiger.skoglund@gmail.com>
2025-11-26 22:52:34 +0100

    github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({¨
+    site: 'https://sk0glund.github.io',
+    base: '/portfolioastro'
+});
